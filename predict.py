8bdb241c71cd65542b54db5d376f53bd945ec74e
Ray Zapata <stephin.ray.zapata@gmail.com>
2021-08-02 19:26:53 -0500

    Create predict.py to predict new readmes

---
+++ predict.py
@@ -0,0 +1,77 @@
+#Z0096
+
+
+####
+####
+####
+####  Run "python predict.py" on command line and pass the location of a readme file.
+####                 Can be a file path or URL to raw GitHub readme
+####
+####
+####
+####
+
+
+import re
+import pickle
+
+from prepare import lemmatize, remove_stopwords, remove_stopwords, basic_clean
+from wrangle import open_pickles
+
+def remove_code_snippets(readme):
+    '''
+    Takes argument string and removes obvious HTML and markup code
+    present in the readme before returning
+    '''
+
+    # remove HTML and markdown code
+    readme = re.sub(r'(\<.+\>)', '', readme)
+    readme = re.sub(r'!?(\[.+\])(\(.+\))?', '', row)
+    # remove coded spaces and page breaks
+    readme = re.sub(r'(\n+)', ' ', row)
+    readme= re.sub(r'(&nbsp;+)', '', row)
+    # remove naked hyperlinks
+    readme = re.sub(r'http\S+', '', row)
+    # replaced hyphens with spaces
+    readme = re.sub(r'-', ' ', row)
+    return readme
+
+def predict_readme_lang(readme):
+    '''
+    Takes in path and file name of README document and prints
+    the predicted language and the probability.
+    '''
+
+    # clean, tokenize, remove stopwords, and lematize
+    readme = lemmatize(remove_stopwords(remove_stopwords(basic_clean(readme))))
+    # open fitted tfidf object
+    tfidf = open_pickles('tfidf')
+    readme = tfidf.transform([readme])
+    # open fitted model object
+    model = open_pickles('model')
+    pred = model.predict(readme)
+    prob = model.predict_proba(readme)
+    # make variable to corresponding langauge to target class
+    if pred == 0:
+        lang = 'Python'
+        prob = prob[0][0]
+    elif pred == 1:
+        lang = 'JavaScript'
+        prob = prob[0][1]
+    elif pred == 2:
+        lang = 'Jupyter'
+        prob = prob[0][2]
+    elif pred == 3:
+        lang = 'HTML'
+        prob = prob[0][3]
+    elif pred == 4:
+        lang = 'R'
+        prob = prob[0][4]
+    else:
+        lang = 'TypeScript'
+        prob = prob[0][5]
+    print(f'\n\nThe provided README is predicted as {lang} with {prob:.2%} probability.\n\n')
+
+
+readme = input('Provide location of readme document: ')
+predict_readme_lang(str(readme))
